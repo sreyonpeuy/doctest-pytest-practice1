a380f99cf5c7a32b2fe570a7761ff4f30f514f00
sreyonpeuy <speuy78@cmc.edu>
2026-09-17 20:29:40 -0700

    fixed markdown

--- src/markdown.py
+++ src/markdown.py
@@ -1,7 +1,9 @@
 '''
 All the functions in this file convert markdown syntax into html.
-Implementing these functions will give you practice learning the correct markdown syntax.
+Implementing these functions will give you practice learning
+the correct markdown syntax.
 '''
+
 
 def compile_italic_underscore(line):
     '''
@@ -19,7 +21,8 @@ def compile_italic_underscore(line):
     '_'
     >>> compile_italic_underscore('_a_ and _b_')
     '<i>a</i> and <i>b</i>'
-    >>> compile_italic_underscore('_a_ and _b')          # odd count: last one is literal
+    >>> compile_italic_underscore('_a_ and _b')
+    # odd count: last one is literal
     '<i>a</i> and _b'
     >>> compile_italic_underscore('no underscores here')
     'no underscores here'
@@ -58,17 +61,27 @@ def compile_links(line):
     Add <a> tags.
 
     HINT:
-    The links and images are potentially more complicated because they have many types of delimeters: `[]()`.
-    These delimiters are not symmetric, however, so we can more easily find the start and stop locations using the strings find function.
+    The links and images are potentially more complicated
+    because they have many types of delimeters: `[]()`.
+    These delimiters are not symmetric, however, so we can more easily
+    find the start and stop locations using the strings find function.
 
-    >>> compile_links('Click on the [course webpage](https://github.com/mikeizbicki/cmc-csci040)!')
-    'Click on the <a href="https://github.com/mikeizbicki/cmc-csci040">course webpage</a>!'
-    >>> compile_links('[course webpage](https://github.com/mikeizbicki/cmc-csci040)')
-    '<a href="https://github.com/mikeizbicki/cmc-csci040">course webpage</a>'
-    >>> compile_links('this is wrong: [course webpage]    (https://github.com/mikeizbicki/cmc-csci040)')
-    'this is wrong: [course webpage]    (https://github.com/mikeizbicki/cmc-csci040)'
-    >>> compile_links('this is wrong: [course webpage](https://github.com/mikeizbicki/cmc-csci040')
-    'this is wrong: [course webpage](https://github.com/mikeizbicki/cmc-csci040'
+    >>> compile_links('Click on the [course webpage](https:
+        //github.com/mikeizbicki/cmc-csci040)!')
+    'Click on the <a href="https://github.com/mikeizbicki
+    /cmc-csci040">course webpage</a>!'
+    >>> compile_links('[course webpage](https://github.com/
+    mikeizbicki/cmc-csci040)')
+    '<a href="https://github.com/mikeizbicki/
+    cmc-csci040">course webpage</a>'
+    >>> compile_links('this is wrong: [course webpage]
+    (https://github.com/mikeizbicki/cmc-csci040)')
+    'this is wrong: [course webpage]    (https:/
+    /github.com/mikeizbicki/cmc-csci040)'
+    >>> compile_links('this is wrong: [course webpage]
+    (https://github.com/mikeizbicki/cmc-csci040')
+    'this is wrong: [course webpage](https:/
+    /github.com/mikeizbicki/cmc-csci040'
     >>> compile_links('[a](1) and [b](2)')
     '<a href="1">a</a> and <a href="2">b</a>'
     >>> compile_links('(parens) then [t](u)')
